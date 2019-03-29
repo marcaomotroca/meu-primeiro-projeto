48871785a5fb015390c27e46da6ec16c7251bb55
Marcos Roberto Nunes <217414@netmaster.intra>
2019-03-29 11:43:25 -0300

    Alteracao para enviar para o github

--- src/main/java/com/mnunes/api/projetospring/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/com/mnunes/api/projetospring/MeuPrimeiroProjetoSpringApplication.java
@@ -1,4 +1,4 @@
-package com.mnunes.api.projetospring;
+ package com.mnunes.api.projetospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class MeuPrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
 	}
 
 }
